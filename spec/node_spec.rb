339b65bc9acf1ff404048625969f0a1432613f0c
Ian Lyell <ian.lyell@yahoo.com>
2023-05-11 17:42:38 -0500

    Setup intial node test

---
+++ spec/node_spec.rb
@@ -0,0 +1,19 @@
+require ".lib/node"
+
+RSpec.describe do
+    it 'exist' do
+        node = Node.new
+
+        expect(node).to be_a(Node)
+    end
+    it 'takes a data argument' do
+        node = Node.new("plop")
+
+        expect(node.data).to eq("plop")
+    end
+    it 'holds a next_node value' do
+        node = Node.new("plop")
+
+        expect(node.next_node).to eq(nil)
+    end
+end
