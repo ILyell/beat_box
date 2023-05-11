ed0ab5b0d8238dddd6204829228efc34e09dfa17
Ian Lyell <ian.lyell@yahoo.com>
2023-05-11 17:57:23 -0500

    Add first LinkedList test

---
+++ spec/linkedlist_spec.rb
@@ -0,0 +1,35 @@
+require "./lib/node"
+require "./lib/linkedlist"
+
+RSpec.describe do 
+    it 'exist' do
+        list = LinkedList.new
+
+        expect(list).to be_a(LinkedList)
+    end
+    it 'holds a head node value' do
+        list = LinkedList.new
+
+        expect(list.head).to eq(nil)
+    end
+    it 'Can add a node the the front of the list' do
+        list = LinkedList.new
+
+        list.head
+        expect(list.head).to eq(nil)
+        list.append("drop")
+        expect(list.node.data).to eq("drop")
+    end
+    it 'can count the nodes contained' do
+        list = LinkedList.new
+        list.append("drop")
+
+        expect(list.count).to eq(1)
+    end
+    it 'Can put the data into a string' do
+        list = LinkedList.new
+        list.append("drop")
+        
+        expect(list.to_string).to eq("doop")
+    end
+end
