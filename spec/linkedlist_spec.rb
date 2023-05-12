e59cd8e9bf0be324b18a74f4c7af9ad93104cc50
Ian Lyell <ian.lyell@yahoo.com>
2023-05-11 20:50:18 -0500

    Add more test for LinkedList

--- spec/linkedlist_spec.rb
+++ spec/linkedlist_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe do
 
         expect(list.head).to eq(nil)
     end
-    it 'Can add a node the back of the list' do
+    it 'Can add a node to the back of the list' do
         list = LinkedList.new
 
         list.head
@@ -38,5 +38,27 @@ RSpec.describe do
         list.append("drop")
         
         expect(list.to_string).to eq("blop drop")
+    end
+    it 'Can add a node to the front of the list' do
+        list = LinkedList.new
+
+        list.head
+        expect(list.head).to eq(nil)
+        list.prepend("drop")
+        expect(list.head.data).to eq("drop")
+        list.prepend("bop")
+        expect(list.head.data).to eq("bop")
+        expect(list.head.next_node.data).to eq("drop")
+    end
+    it 'Can insert data at a set place in the list' do
+        list = LinkedList.new
+
+        list.prepend("drop")
+        list.prepend("bop")
+        list.insert(1, "plop")
+
+        expect(list.data).to eq("bop")
+        expect(list.next_node.data).to eq("plop")
+        expect(list.next_node.next_node.data).to eq("drop")
     end
 end
