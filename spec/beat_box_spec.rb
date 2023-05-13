37f50555294dbbc28f61cdc65374ed5e252d55c9
Ian Lyell <ian.lyell@yahoo.com>
2023-05-12 21:16:06 -0500

    Add test for prepend and all methods

--- spec/beat_box_spec.rb
+++ spec/beat_box_spec.rb
@@ -16,6 +16,7 @@ RSpec.describe do
         expect(bb.list).to be_a(LinkedList)
         expect(bb.list.head).to eq(nil)
     end
+
     it "can appened multible nodes at once" do
         bb = BeatBox.new
         bb.append("deep doo ditt")
@@ -27,5 +28,22 @@ RSpec.describe do
 
         expect(bb.count).to eq(6)
         expect(bb.list.to_string).to eq("deep doo ditt woo hoo shu")
+    end
+
+    it 'can preprened multiple nodes at once' do
+        bb = BeatBox.new
+        bb.append("deep doo ditt")
+        bb.prepend("woo hoo shu")
+
+        expect(bb.list.to_string).to eq("woo hoo shu deep doo ditt")
+        expect(bb.count).to eq(6)
+    end
+
+    it 'can show all data in the list' do
+        bb = BeatBox.new
+        bb.append("deep doo ditt")
+        bb.prepend("woo hoo shu")
+
+        expect(bb.all).to eq("woo hoo shu deep doo ditt")
     end
 end
