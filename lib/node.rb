97b0e07de46ffc140832587dd0f3cdd58b171c79
Ian Lyell <ian.lyell@yahoo.com>
2023-05-15 15:44:06 -0500

    Change attr_acessor to next_node method for better insulation

--- lib/node.rb
+++ lib/node.rb
@@ -1,12 +1,15 @@
 class Node
 
-    attr_reader :data
-    attr_accessor :next_node
+    attr_reader :data, :next_node
 
     # Basic node initialization
 
     def initialize(data)
         @data = data
         @next_node = nil
+    end
+
+    def next_node(node= nil)
+        @next_node = node
     end
 end
